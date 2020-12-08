cda7618bbc79812baaac18dc17533c49354a5029
新西兰小母牛 <1906443298@qq.com>
2020-12-08 15:07:25 +0800

    222

--- Model/Student.cs
+++ Model/Student.cs
@@ -6,7 +6,7 @@ namespace Model
 {
    public class Student
     {
-        public int SId { get; set; }
+        public int SIdddd { get; set; }
         public string SName { get; set; }
         public int SAge { get; set; }
         public string SHome { get; set; }
